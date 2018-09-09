738f728b99f9cb2958678f8fe1dbdb4d0afe4b08
Aurolux <36804393+Aurolux@users.noreply.github.com>
2018-09-09 17:46:11 -0500

    Create a config file using python

---
+++ config-example.py
@@ -0,0 +1,12 @@
+info = {
+  
+  # Bot's username, password (must be registered)
+  'username': 'exampleuser',
+  'password': 'examplepass',
+  
+  # Rooms to join. To join one room, remove brackets.
+  'joinRooms': ['room1','room2'],
+  
+  # User(s) who surpass permission checks. To put one owner, remove brackets.
+  'owners': ['owner1', 'owner2']
+}
